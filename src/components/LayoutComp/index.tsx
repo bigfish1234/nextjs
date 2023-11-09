d57620944788ef4c714042762f384b5cf553c4be
yunyun <yuyunyun@boulderaitech.com>
2023-11-09 17:22:22 +0800

    feat: 咨询

--- src/components/LayoutComp/index.tsx
+++ src/components/LayoutComp/index.tsx
@@ -1,13 +1,25 @@
+import Image from "next/image";
 import FooterComp from "../FooterComp";
 import SwiperHeader from "../SwiperHeader";
-// import joinImg from "@/images/infomation/info-center.png";
+import guidenceImg from "@/images/eimos/guidance.png";
+import styles from "./index.module.css";
 
 const LayoutComp = ({ imgUrl, children }: any) => {
   return (
     <div>
+      {/* 轮播图 */}
       <SwiperHeader imgUrl={imgUrl} />
       <slot>{children}</slot>
+      {/* footer */}
       <FooterComp />
+      {/* 咨询 */}
+      <Image
+        src={guidenceImg}
+        alt="gudience"
+        width={38}
+        height={38}
+        className={styles["guide-service"]}
+      />
     </div>
   );
 };
