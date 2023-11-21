d739d85da859e26fecae31e6f650531450fef54f
yunyun <yuyunyun@boulderaitech.com>
2023-11-21 20:03:20 +0800

    feat: 提交表单并发送邮箱

--- src/pages/api/submit.ts
+++ src/pages/api/submit.ts
@@ -1,17 +1,45 @@
-import { NextApiResponse, NextApiRequest } from 'next'
+import { NextApiResponse, NextApiRequest } from "next";
+import db from "../db";
 
-
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse,
+  res: NextApiResponse
 ) {
   const body = req?.body;
-  // todo 拿到body 数据，存到数据库
-  const result  = {status: true, id: '1111'}; // 处理结果
+  const { name, email, company, phone, detail } = body;
 
-  // 操作完数据库，返回请求
-  if(!result.status){
-    return res.status(500);
+  try {
+    await new Promise((resolved, reject) => {
+      // 插入数据
+      db.query(
+        {
+          sql: `INSERT INTO spzn.CONSULTATION_INFO (name,email,company,phone,detail) VALUES (?,?,?,?,?);`,
+          values: [name, email, company, phone, detail],
+        },
+        (err: any, result: any) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolved(result);
+          }
+        }
+      );
+    });
+
+    res.statusCode = 200;
+    res.json({
+      code: 200,
+      data: null,
+      success: true,
+      message: "提交成功",
+    });
+  } catch (error) {
+    res.statusCode = 500;
+    res.json({
+      code: 500,
+      data: null,
+      message: "提交失败!",
+      success: false,
+    });
   }
-  return res.status(200).json({...result});
 }
